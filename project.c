6dd8ed87f3daa46a70bcd8dbd0554c24b547ba1c
Ipuvi Mishra <ipuvi98@gmail.com>
2018-04-12 00:00:03 +0530

    Second commit

---
+++ project.c
@@ -0,0 +1,7 @@
+/*WAP to perform priority based schedulling which can be handled from 10am to 12pm only and 
+at the end of session calculate total time spent and average query time.
+*/
+
+#include<stdio.h>
+#include<unistd.h>
+
